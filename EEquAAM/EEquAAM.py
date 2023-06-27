7bad8e3e85a3be5793b37bd84545405aff0d61e4
MarcosLaffitte <marcoslaffitte@gmail.com>
2023-06-27 10:56:05 +0200

    EEquAAM comparison of atom-to-atom maps

--- EEquAAM/EEquAAM.py
+++ EEquAAM/EEquAAM.py
@@ -96,10 +96,17 @@
 #                                                                              #
 #    * WARNING! annotations of the type "|1^...|" will be silently removed     #
 #                                                                              #
+#  --------------------------------------------------------------------------  #
 #                                                                              #
-#  - DISCLAIMER: This code is provided "AS IS". You may use it uder your own   #
-#    risk and consideration. This script was built for research purposes and   #
-#    its developers claim no responsabilty on further third-party use.         #
+# - LICENSE:                                                                   #
+#                                                                              #
+#   This file is part of the work published in                                 #
+#            https://match.pmf.kg.ac.rs/issues/m90n1/m90n1_75-102.html         #
+#   and it is released under                                                   #
+#            MIT License Copyright (c) 2023 Marcos E. González Laffitte        #
+#   See LICENSE file in                                                        #
+#            https://github.com/MarcosLaffitte/EEquAAM                         #
+#   for full license details.                                                  #
 #                                                                              #
 ################################################################################
 
